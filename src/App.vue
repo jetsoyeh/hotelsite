93114d96716793449c5f28930469d94223b0396b
jetso <jetsoyeh@gmail.com>
2020-11-25 21:32:55 +0800

    新增主選單與側邊選單

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,87 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+  <v-app id="inspire">
+    <v-container fluid>
+      <!--側邊欄-->
+      <v-navigation-drawer
+        temporary
+        v-model="sidebar"
+        app
+        clipped
+        class="red lighten-3"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+        <v-list nav dense>
+          <v-list-item>
+            <v-list-item-content>
+              <v-img src="@/assets/logo.png"></v-img>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+          <div v-for="item in menuItems" :key="item.title">
+            <v-list-item to="item.link">
+              <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </div>
+        </v-list>
+      </v-navigation-drawer>
+      <!--主選單-->
+      <v-app-bar dark class="red lighten-3">
+        <v-app-bar-nav-icon
+          @click.stop="sidebar = !sidebar"
+          class="hidden-sm-and-up"
+        ></v-app-bar-nav-icon>
+        <v-toolbar-title>
+          <router-link to="/" tag="span" style="cursor: pointer"
+            >SunRaise</router-link
+          >
+        </v-toolbar-title>
+        <v-spacer />
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn
+            text
+            v-for="item in menuItems"
+            :key="item.title"
+            :to="item.link"
+          >
+            <v-icon left>{{ item.icon }}</v-icon>
+            {{ item.title }}
+          </v-btn>
+        </v-toolbar-items>
+        <v-spacer />
+      </v-app-bar>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  name: "App",
+  data() {
+    return {
+      sidebar: false,
+      menuItems: [
+        { icon: "mdi-home", title: "日出之美", link: "/" },
+        { icon: "mdi-room-service", title: "房型介紹", link: "/room" },
+        {
+          icon: "mdi-backspace-reverse-outline",
+          title: "日出空間",
+          link: "/publicspace",
+        },
+        { icon: "mdi-google-maps", title: "周邊景點", link: "/attractions" },
+        { icon: "mdi-new-box", title: "最新消息", link: "/new" },
+        { icon: "mdi-contacts", title: "聯絡我們", link: "/contacts" },
+      ],
+    };
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
+<style lang="sass">
+@import '~vuetify/src/styles/main.sass'
+</style>
